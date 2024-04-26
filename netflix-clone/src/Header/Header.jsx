46713aa0f0d2b0676b6be67152e5246b8205b87f
eualx <eualx14j@gmail.com>
2024-04-26 23:10:31 +0300

    commit-footerheader

--- netflix-clone/src/Header/Header.jsx
+++ netflix-clone/src/Header/Header.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import "./Header.css"
 import Logo from "../assets/image/Netflixlogo.jpg"
-// import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 const Header = () => {
   return (
     <div className='header_outer_container'>
       <div className='header_container'>
         <div className='header_left'>
-<ul>
-  <li><img src={Logo} alt="NetFlix Logo" width="100" /></li>
-  <li>Netflix</li>
-  <li>Home</li>
-  <li>TVShows</li>
-  <li>Movies</li>
-  <li>Latest</li>
-  <li>MyList</li>
-  <li>Browse by Language</li>
-</ul>
+          <ul>
+            <li><img src={Logo} alt="NetFlix Logo" width="100" /></li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Language</li>
+          </ul>
         </div>
         <div className='header_right'>
-<ul>
-  <li><SearchOutlinedIcon/></li>
-  {/* <li><AccountBoxIcon/></li>
-  <li><ArrowDropDownIcon/></li> */}
-</ul>
+          <ul>
+            <li><SearchIcon /></li>
+            <li><NotificationsNoneIcon /></li>
+            <li><AccountBoxIcon /></li>
+            <li><ArrowDropDownIcon /></li>
+          </ul>
         </div>
       </div >
     </div>
